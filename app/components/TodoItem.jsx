047c0691bbd469e174aa275414441ef2cfb3ec56
Chris Bryant <cbryant@caxy.com>
2016-01-20 12:15:29 -0500

    Refactored TodoItem into a functional component. Added TodoList as a functional component. Refactored bits of TodoApp component to pass props to the child components properly.

--- app/components/TodoItem.jsx
+++ app/components/TodoItem.jsx
@@ -1,33 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoActions from './../actions/TodoActions';
 
-class TodoItem extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.toggleCompleted = this.toggleCompleted.bind(this);
-    this.removeTodo = this.removeTodo.bind(this);
-  }
-
-  toggleCompleted(e) {
-    TodoActions.toggleCompleted(this.props.todo.id);
-  }
-
-  removeTodo(e) {
-    TodoActions.destroy(this.props.todo.id);
-  }
-
-  render() {
-    let className = this.props.todo.completed ? 'completed' : '';
-
-    return (
-      <li className={className}>
-        <p onClick={this.toggleCompleted}>{this.props.todo.text}</p>
-        <button className="delete" onClick={this.removeTodo}>&#x02A2F;</button>
-      </li>
-    );
-  }
-}
+var TodoItem = ({
+  onClick,
+  onDelete,
+  text,
+  completed
+}) => (
+  <li className={completed ? 'completed' : ''}>
+    <p onClick={onClick}>{text}</p>
+    <button className="delete" onClick={onDelete}>
+      &#x02A2F;
+    </button>
+  </li>
+);
 
 export default TodoItem;

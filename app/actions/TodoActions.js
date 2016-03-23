641bd17ba76db4531c468859ec4415ca977ec7f3
Chris Bryant <cbryant@caxy.com>
2016-03-23 11:00:13 -0400

    Added start script to package.json. Removed class syntax from TodoActions.

--- app/actions/TodoActions.js
+++ app/actions/TodoActions.js
@@ -1,27 +1,27 @@
 import TodoDispatcher from './../dispatcher/TodoDispatcher';
 import * as ActionTypes from './../constants/actionTypes';
 
-class TodoActions {
-  create(text) {
+let TodoActions = {
+  create: (text) => {
     TodoDispatcher.dispatch({
       action: ActionTypes.TODO_CREATE,
       text: text
     });
-  }
+  },
 
-  destroy(id) {
+  destroy: (id) => {
     TodoDispatcher.dispatch({
       action: ActionTypes.TODO_DESTROY,
       id: id
     });
-  }
+  },
 
-  toggleCompleted(id) {
+  toggleCompleted: (id) => {
     TodoDispatcher.dispatch({
       action: ActionTypes.TODO_TOGGLE_COMPLETED,
       id: id
     });
   }
-}
+};
 
-export default new TodoActions();
+export default TodoActions;
